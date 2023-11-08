66b567f7752fb122a453032f386112455ceb4e9a
rohit-chadda1803 <72185610+rohit-chadda1803@users.noreply.github.com>
2023-11-08 23:03:41 +0530

    Add files via upload

---
+++ input-4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// created by Rohit Chadda 
+// input - output , program-4
+
+
+class inp_float {
+    public static void main(String[] args){
+
+        // taking float 
+        System.out.println("taking float input from the user");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter no. -");
+        float a = sc.nextFloat();
+
+        System.out.println("the entered float value is - "+a);
+
+
+        sc.close();
+
+
+    }
+
+    
+
+
+
+}
